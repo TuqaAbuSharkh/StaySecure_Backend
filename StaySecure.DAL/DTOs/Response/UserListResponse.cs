99a7079ff110b5bc230f3143417bb6faf7beee0c
TuqaAbuSharkh <abusharktuq@gmail.com>
2026-03-18 21:11:10 +0200

    user and userTranslations

---
+++ StaySecure.DAL/DTOs/Response/UserListResponse.cs
@@ -0,0 +1,20 @@
+﻿using StaySecure.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaySecure.DAL.DTOs.Response
+{
+    public class UserListResponse
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsBlocked { get; set; }
+        public List<string> Roles { get; set; }
+
+    }
+}
---
+++ StaySecure.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaySecure.DAL.Models
+{
+    public enum GenderEnum
+    {
+        Male = 1,
+        Female = 2
+    }
+
+
+    public class ApplicationUser :IdentityUser
+    {
+
+       public List<ApplicationUserTranslations> Translations { get; set; }
+        public int? Age { get; set; }
+        public int TotalScore { get; set; } = 0;
+
+        public GenderEnum? Gender { get; set; } = (GenderEnum?)1;
+
+
+        public string? CodeResetPassword { get; set; }
+        public DateTime? PasswordResetCodeExpiry { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
